573166363393bfb7b0d9710f28e8f8a9989e4ca6
AYAN766666 <163290028+AYAN766666@users.noreply.github.com>
2024-03-28 08:14:05 -0700

    change

--- app.ts
+++ app.ts
@@ -3,21 +3,22 @@ import inquirer from "inquirer";
 
 
 const answer = await inquirer.prompt([
-    {message:"Enter your first number",type :"number", name: "firstNumber"}, 
-    {message:"Enter your second number",type:"number", name: "secondNumber"}, 
+    {message:"Enter your first number",type :"number", name: "NumberOne"}, 
+    {message:"Enter your second number",type:"number", name: "NumberTwo"}, 
     {message: "Select one of operators to perform action", type: "list",name:"operator", choices:["ADDITION","SUBTRACTION","MULTIPLICATION","DIVISION"]}, 
 
 ])
     
 //CONDITIONAL STATEMENT 
-if(answer.operator === "ADDITION") {
-    console.log(answer.firstNumber + answer.secondNumber); 
-}else if (answer.operator === "SUBTRACTION"){
-    console.log(answer.firstNumber - answer.secondNumber);
+if(answer.operator === "ADDITION") {; 
+    console.log(answer.numberOne + answer.numberTwo)
 }else if (answer.operator === "MULTIPLICATION"){
-    console.log(answer.firstNumber * answer.secondNumber)
+    console.log(answer.NumberOne * answer.sNumberTwo)
 }else if (answer.operator === "DIVISION"){
-    console.log(answer.firstNumber / answer.secondNumber);
+    console.log(answer.numberOne / answer.NumberTwo);
+}else if (answer.numberOne === "SUBTRACTION"){
+
+    console.log(answer.numberOne - answer.numberTwo);
 }
 else {"please select correct operator"}
 
